293d8848c62ee4e42ebd32ff65c905a353f82f85
Miyakawa Takuya <omyakawa@gmail.com>
2021-06-22 03:48:24 +0900

    完成

--- Sound_of_Dogs/ContentView.swift
+++ Sound_of_Dogs/ContentView.swift
@@ -17,35 +17,77 @@ struct ContentView: View {
                 .resizable()
                 .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
                 .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
-            //水平にレイアウト
-            HStack{  //1行目
-                //dog1
-                Button(action:{
-                    //ボタンタップしたときのアクション
-                    //定数定義した中のdog1関数を発火
-                    soundPlayer.dog1_Play()
-                }){
-                    Image("dog1")
-                        //ボタンの画像がカラー画像になるように指定している
-                        .renderingMode(.original)
-                        .resizable()
-                        .frame(width:190,height:190) //フレームサイズを指定
+            VStack{//３行で処理したい
+                //水平にレイアウト
+                HStack{  //1行目
+                    //dog1
+                    Button(action:{
+                        //ボタンタップしたときのアクション
+                        //定数定義した中のdog1関数を発火
+                        soundPlayer.dog1_Play()
+                    }){
+                        Image("dog1")
+                            //ボタンの画像がカラー画像になるように指定している
+                            .renderingMode(.original)
+                            .resizable()
+                            .frame(width:190,height:190) //フレームサイズを指定
+                    }  //dog1ここまで
                     
-                }  //dog1ここまで
+                    //dog2
+                    Button(action:{
+                        soundPlayer.dog2_Play()
+                    }){
+                        Image("dog2")
+                            .renderingMode(.original)
+                            .resizable()
+                            .frame(width:180,height:190) //フレームサイズを指定
+                    }  //dog2ここまで
+                }//HStackここで終わり
                 
-                //dog2
-                Button(action:{
-                    //ボタンタップしたときのアクション
-                }){
-                    Image("dog2")
-                        .renderingMode(.original)
-                        .resizable()
-                        .frame(width:180,height:190) //フレームサイズを指定
-
-                }  //dog2ここまで
+                HStack{  //2行目
+                    Button(action:{//dog3
+                        soundPlayer.dog3_Play()
+                    }){
+                        Image("dog3")
+                            .renderingMode(.original)
+                            .resizable()
+                            .frame(width:190,height:190)
+                    }  //dog3ここまで
+                    
+                    //dog4
+                    Button(action:{
+                        soundPlayer.dog4_Play()
+                    }){
+                        Image("dog4")
+                            .renderingMode(.original)
+                            .resizable()
+                            .frame(width:190,height:190)
+                    }  //dog4ここまで
+                }//HStack2ここで終わり
+                
+                HStack{  //3行目
+                    Button(action:{//dog5
+                        soundPlayer.dog5_Play()
+                    }){
+                        Image("dog5")
+                            .renderingMode(.original)
+                            .resizable()
+                            .frame(width:190,height:190)
+                    }  //dog5ここまで
+                    
+                    //dog6
+                    Button(action:{
+                        soundPlayer.dog6_Play()
+                    }){
+                        Image("dog6")
+                            .renderingMode(.original)
+                            .resizable()
+                            .frame(width:190,height:190)
+                    }  //dog6ここまで
+                }//HStack3 ここで終わり
                 
                 
-            }//HStackここで終わり
+            }//VStackここまで
         }//ZStackここで終わり
     } //body
 } //ContentView
--- Sound_of_Dogs/SoundPlayer.swift
+++ Sound_of_Dogs/SoundPlayer.swift
@@ -26,9 +26,59 @@ class SoundPlayer: NSObject {  //SoundPlayerクラス
             dog1_Player.play()
         } catch { //ここにエラーの際の処理を記載する
             print("犬の音声でエラーが発生しました")
-        }
+        } //dog1ここまで
     }
     
+    //dog2
+    let dog2m = NSDataAsset(name:"dog2m")!.data
+    var dog2_Player : AVAudioPlayer!
+    func dog2_Play(){
+        do {dog2_Player = try AVAudioPlayer(data: dog2m)
+            dog2_Player.play()
+        } catch {print("犬の音声でエラーが発生しました")
+        } //dogここまで
+    }
     
+    //dog3
+    let dog3m = NSDataAsset(name:"dog3m")!.data
+    var dog3_Player : AVAudioPlayer!
+    func dog3_Play(){
+        do {dog3_Player = try AVAudioPlayer(data: dog3m)
+            dog3_Player.play()
+        } catch {print("犬の音声でエラーが発生しました")
+        } //dogここまで
+    }
+    
+    //dog4
+    let dog4m = NSDataAsset(name:"dog4m")!.data
+    var dog4_Player : AVAudioPlayer!
+    func dog4_Play(){
+        do {dog4_Player = try AVAudioPlayer(data: dog4m)
+            dog4_Player.play()
+        } catch {print("犬の音声でエラーが発生しました")
+        } //dogここまで
+    }
+    
+    //dog5
+    let dog5m = NSDataAsset(name:"dog5m")!.data
+    var dog5_Player : AVAudioPlayer!
+    func dog5_Play(){
+        do {dog5_Player = try AVAudioPlayer(data: dog5m)
+            dog5_Player.play()
+        } catch {print("犬の音声でエラーが発生しました")
+        } //dogここまで
+    }
+
+    //dog6
+    let dog6m = NSDataAsset(name:"dog6m")!.data
+    var dog6_Player : AVAudioPlayer!
+    func dog6_Play(){
+        do {dog6_Player = try AVAudioPlayer(data: dog6m)
+            dog6_Player.play()
+        } catch {print("犬の音声でエラーが発生しました")
+        } //dogここまで
+    }
+
+
     
 }
