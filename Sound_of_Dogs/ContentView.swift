d64d43b329f54da95b1cd8d9400c51673620a929
Miyakawa Takuya <omyakawa@gmail.com>
2021-06-21 21:44:20 +0900

    Initial Commit

---
+++ Sound_of_Dogs/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Sound_of_Dogs
+//
+//  Created by 宮川卓也 on 2021/06/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Sound_of_Dogs/Sound_of_DogsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sound_of_DogsApp.swift
+//  Sound_of_Dogs
+//
+//  Created by 宮川卓也 on 2021/06/21.
+//
+
+import SwiftUI
+
+@main
+struct Sound_of_DogsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
